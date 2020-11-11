924d7dd91d6f5d43ff309b83fddf6ba31f9e1c46
Brayan Arias <BRAYAN@ElectronicaArias.onmicrosoft.com>
2020-11-11 03:21:23 -0500

    guardar cambio

--- BilleteraElectronicaLibreria/Class1.cs
+++ BilleteraElectronicaLibreria/Class1.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace BilleteraElectronicaLibreria
 {
-    public class Class1
+ 
+    public class CuentaCorriente
     {
+        private String numeroCuenta;
+        private String DNI;
+        private Decimal saldo;
 
+        //Todo_revisar si es necesario set
+
+        public decimal Saldo { get => saldo; set => saldo = value; }
+
+        public CuentaCorriente()
+        {
+            
+        }
+        public CuentaCorriente(String numeroCuenta,String DNI,Decimal saldo)
+        {
+            this.numeroCuenta = numeroCuenta;
+            this.DNI = DNI;
+            this.Saldo = saldo;
+        }
+
+     
     }
 }
---
+++ BilleteraElectronicaPrueba/CuentaCorrientePrueba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BilleteraElectronicaLibreria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilleteraElectronicaPrueba
+{
+    [TestClass]
+    public class CuentaCorrientePrueba
+    {
+        [TestMethod]
+        public void cuentaCorriente_aperturaCuentaConSaldoCero_saldoIgualCero()
+        {
+            //Arrange
+            String numeroCuenta = "44445";
+            String DNI = "45675789";
+            Decimal saldoInicialEsperado = 0;
+            Decimal saldoInicialActual;
+
+
+            //do
+            CuentaCorriente cuenta = new CuentaCorriente(numeroCuenta, DNI, 0);
+            saldoInicialActual = cuenta.Saldo;
+
+            //Assert
+            Assert.AreEqual(saldoInicialEsperado, saldoInicialActual, "Error: El Saldo no es cero");
+        }
+    }
+}
